084442882c3bf8e2a76f9d6c73941bfb172395a0
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-03-17 16:11:30 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+public class Test
+{
+    private int Mid;
+    private int Final;
+
+
+    public int MyMid
+    {
+        get
+        {
+            return Mid;
+        }
+        set
+        {
+            if (value < 0 || value > 100)
+            {
+                Mid = 0;
+            }
+            else
+            {
+                Mid = value;
+            }
+        }
+    }
+
+    public int MyFinal
+    {
+        get
+        {
+            return Final;
+        }
+        set
+        {
+            if (value < 0 || value > 100)
+            {
+                Final = 0;
+            }
+            else
+            {
+                Final = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Class_0317_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1 = new Test();
+
+            std1.MyMid = 101;
+            std1.MyFinal = -5;
+
+            Console.WriteLine("期中考" + std1.MyMid.ToString());
+            Console.WriteLine("期中考" + std1.MyFinal.ToString());
+
+
+        }
+    }
+}
